3f55b4f62e210f09722382cca9a5ab8c95652146
tannerhelmsllc <tannerhelmsllc@gmail.com>
2024-01-14 15:43:58 -0700

    first commit

---
+++ Spy Chat/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import UsernameBox from './componets/usernameBox'
+
+function App() {
+  return (
+    <>
+            <div>
+              <UsernameBox />
+            </div>
+    </>
+  )
+}
+
+export default App
